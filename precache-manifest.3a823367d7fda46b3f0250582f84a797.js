9ffac4be375cd34da06003e0f139f247f463e69d
Zhenyuan-Li <vincent0lee95@gmail.com>
2022-10-18 21:32:45 +0800

    Updates

---
+++ precache-manifest.3a823367d7fda46b3f0250582f84a797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b599121632cf03dd099e9bf8cf66b7a3",
+    "url": "/weather-app-frontend/index.html"
+  },
+  {
+    "revision": "02c4cc8373478f18e711",
+    "url": "/weather-app-frontend/static/css/main.53dc383b.chunk.css"
+  },
+  {
+    "revision": "a82ab277e037da75ad7a",
+    "url": "/weather-app-frontend/static/js/2.2ac357fa.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/weather-app-frontend/static/js/2.2ac357fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c4cc8373478f18e711",
+    "url": "/weather-app-frontend/static/js/main.81fa9275.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/weather-app-frontend/static/js/main.81fa9275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "603d0c65a2f8d3bd4d95",
+    "url": "/weather-app-frontend/static/js/runtime-main.08f78632.js"
+  }
+]);
